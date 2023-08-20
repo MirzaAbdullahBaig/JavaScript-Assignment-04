f83ffccf6f5b655e27cb5d746ff5ceccd31a918b
Mirza Abdullah Baig <abaig7921gmail.com>
2023-08-20 23:54:10 +0500

    Task 06 Done

--- Task 06/Task06.js
+++ Task 06/Task06.js
@@ -1,1 +1,12 @@
 // Task 06: Write a program that simulates a dice using random() method of JS Math class. Display the value of dice in your browser.
+
+var num = Math.random();
+var num2 = (num * 6) + 1;
+var dice1 = Math.floor(num2);
+
+var newNum = Math.random();
+var newNum2 = (newNum * 6) + 1;
+var dice2 = Math.floor(newNum2);
+
+document.write("Random Dice Value: " + dice1 + "<br/><br/>");
+document.write("Random Dice Value: " + dice2);
