e3ea71df1f1c707837b2a477e2d0ff49c3065ff7
Mirza Abdullah Baig <abaig7921gmail.com>
2023-08-20 21:04:29 +0500

    Task 01 Done

--- Task 01/Task01.js
+++ Task 01/Task01.js
@@ -1,1 +1,8 @@
 // Task 01: Write a program to replace the “Hyder” to “Islam” in the word “Hyderabad” and display the result in your browser.
+
+var city = "Hyderabad";
+
+var newcity1 = city.replace("Hyder" , "Islam");
+
+document.write("<b> City: " + city + "<br/><br/>");
+document.write("After Replacement: " + newcity1);
