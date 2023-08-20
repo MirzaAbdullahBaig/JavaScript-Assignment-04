4ddedc6a91ea68484ffb95f12e4e185424a9f188
Mirza Abdullah Baig <abaig7921gmail.com>
2023-08-20 23:49:00 +0500

    Task 05 Done

--- Task 05/Task05.js
+++ Task 05/Task05.js
@@ -4,3 +4,14 @@
 // b. round off value of the number
 // c. floor value of the number
 // d. ceil value of the number
+
+var num = prompt("Enter a positive decimal number");
+
+if (num > 0) {
+    document.write("Number is: " + num + "<br/><br/>");
+    document.write("Round of Value: " + Math.round(num) + "<br/><br/>");
+    document.write("Floor Value: " + Math.floor(num) + "<br/><br/>");
+    document.write("Ceil Value: " + Math.ceil(num) + "<br/><br/>");
+} else {
+    document.write ("Please enter a positive number");
+}
