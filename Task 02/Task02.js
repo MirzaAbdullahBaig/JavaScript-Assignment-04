bdc32010c656e3052b11a6e846531936b39b5dea
Mirza Abdullah Baig <abaig7921gmail.com>
2023-08-20 23:23:28 +0500

    Task 02 Done

--- Task 02/Task02.js
+++ Task 02/Task02.js
@@ -1,1 +1,9 @@
 // Task 02: Write a program that converts a string “472” to a number 472. Display the values & types in your browser.
+
+var numInStr = "472";
+var numInNum = parseInt(numInStr)
+
+document.write("Value: " + numInStr + "<br/><br/>");
+document.write("Type: " + typeof numInStr + "<br/><br/>");
+document.write("Value: " + numInNum + "<br/><br/>");
+document.write("Type: " + typeof numInNum);
