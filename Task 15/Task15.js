724d2ca77cffb14667673096663544a0a6ea7b01
Mirza Abdullah Baig <abaig7921gmail.com>
2023-08-21 22:28:14 +0500

    Task 15 Done

--- Task 15/Task15.js
+++ Task 15/Task15.js
@@ -1,1 +1,9 @@
 // Task 15: Write a program to ask the user about his age. Calculate and show his birth year in your browser
+
+var age = prompt("Enter your age");
+var date = new Date();
+
+var birthYear = date.getFullYear() - age;
+
+document.write("Your age is " + age + "<br/><br/>");
+document.write("Your Birth Year is " + birthYear);
