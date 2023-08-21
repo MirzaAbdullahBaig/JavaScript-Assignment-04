ddf8af9446f89515e84960db75e0f938b488a0cc
Mirza Abdullah Baig <abaig7921gmail.com>
2023-08-21 22:19:57 +0500

    Task 12 Done

--- Task 12/Task12.js
+++ Task 12/Task12.js
@@ -1,1 +1,9 @@
 // Task 12: Write a program that shows the message “First fifteen days of the month” if the date is less than the 16th of the month else shows “Last days of the month”.
+
+var date = new Date();
+
+if (date.getDate() <= 15) {
+    document.write("“First fifteen days of the month”");
+} else {
+    document.write("“Last days of the month”");
+}
